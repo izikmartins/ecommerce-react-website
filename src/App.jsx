ddef57088c686ddcfda59611fc0715c7823fab44
Isaac <izikmartins@gmail.com>
2026-04-29 18:01:58 +0100

    added authenticatio

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from './pages/Home';
 import Auth from './pages/Auth';
 import Checkout from './pages/Checkout';
 import Navbar from './components/Navbar';
+import AuthProvider from './context/AuthContext';
 
 
 function App() {   
  
 
   return (
+    <AuthProvider>
    <div className="app">
     <Navbar />
     <Routes>
@@ -18,7 +20,8 @@ function App() {
       <Route path="/checkout" element={<Checkout />}/>
     </Routes>
    </div>
-  )
+   </AuthProvider>
+  );
 }
 
-export default App
+export default App;
